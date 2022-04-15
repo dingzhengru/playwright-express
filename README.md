e1e03662a03120f51a6e90fbb32d9890ebd62cdc
dingzhengru <qws7869vdx@gmail.com>
2022-04-15 15:26:53 +0800

    feat(google-search): update

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # playwright-with-express
+
 測試 playwright + express
+
+## Linux
+
+```shell
+npx playwright install
+chmod 777 -R playwright-with-express/
+chmod 777 -R /root/.npm/_logs
+```
--- tests/google-search.spec.js
+++ tests/google-search.spec.js
@@ -3,7 +3,10 @@ const { test, expect } = require('@playwright/test');
 test.describe('Google', () => {
   test('Search', async ({ page }) => {
     console.log('process.env.REQUEST_BODY', process.env.REQUEST_BODY);
-    const search = JSON.parse(process.env.REQUEST_BODY).q || '';
+    let search = 'default';
+    if (process.env.REQUEST_BODY) {
+      search = JSON.parse(process.env.REQUEST_BODY).q || '';
+    }
 
     await page.goto('https://google.com');
     await page.fill('input[name="q"]', search);
